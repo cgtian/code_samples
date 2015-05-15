3afe0596ccb70857b27f419c42387d5df4a1b1d0
cgtian <chris.tian@nyu.edu>
2015-05-15 01:02:10 -0400

    added [7], fixed [3], changed tempdb names

--- Sample 2 - Algebra 2 Analysis 4.15.sql
+++ Sample 2 - Algebra 2 Analysis 4.15.sql
@@ -19,21 +19,26 @@ code outline
 [4] active cohort r students
 
 [5] various demographic breakdowns for active cohort r students who have taken or are taking algebra 2
-	[5a] race breakdown for active cohort r students who have taken or are taking algebra 2 
+	[5a] ethnic breakdown for active cohort r students who have taken or are taking algebra 2 
 	[5b] iep status breakdown for active cohort r students who have taken or are taking algebra 2
 	[5c] ell status breakdown for active cohort r students who have taken or are taking algebra 2
 
 [6] various demographic breakdowns for active cohort r students
-	[6a] race breakdown for active cohort r students
+	[6a] ethnic breakdown for active cohort r students
 	[6b] iep status breakdown for active cohort r students
 	[6c] ell status breakdown for active cohort r students
 
+[7] various measures of algebra 2 availability for cohort r students
+	[7a] percentage of schools offering high school level alg2 in 2014
+	[7b] percentages of active cohort r students who have taken or are taking alg2 by ethnicity
+	[7c] percentages of active cohort r students who have taken or are taking alg2 by iep status
+	[7d] percentages of active cohort r students who have taken or are taking alg2 by ell status
+
 deliverables
-% of schools offering high school level alg2 in 2014: [1]/[2]
-% of active cohort r students who have taken or are taking alg2: [3cii]/[4]
-%s of active cohort r students who have taken or are taking alg2 by race: [5a]/[6a]
-%s of active cohort r students who have taken or are taking alg2 by iep status: [5b]/[6b]
-%s of active cohort r students who have taken or are taking alg2 by ell status: [5c]/[6c]
+percentage of schools offering high school level alg2 in 2014: [7a]
+percentages of active cohort r students who have taken or are taking alg2 by race: [7b]
+percentages of active cohort r students who have taken or are taking alg2 by iep status: [7c]
+percentages of active cohort r students who have taken or are taking alg2 by ell status: [7d]
 */
 
 
@@ -41,8 +46,12 @@ deliverables
 
 
 -----[1] number of schools offering high school level alg2 in 2014
+if object_id('tempdb..#ct_schools_alg2') is not null drop table #ct_schools_alg2
+
 select
-count(distinct sr.numericschooldbn) as ct_alg2_hs
+count(distinct sr.numericschooldbn) as ct_schools_alg2
+
+into #ct_schools_alg2
 
 from student_request as sr
 
@@ -68,8 +77,12 @@ and substring(s.schooldbn,4,3)!='444'
 
 
 -----[2] number of schools scheduling high school course(s) in 2014
+if object_id('tempdb..#ct_schools') is not null drop table #ct_schools
+
 select
-count(distinct school_dbn) as ct_hs
+count(distinct school_dbn) as ct_schools
+
+into #ct_schools
 
 from bio_data
 
@@ -85,12 +98,15 @@ and substring(school_dbn,4,3)!='444'
 
 -----[3] active cohort r students who have taken or are taking algebra 2
 ---[3a] active cohort r students scheduled for alg2 in 2014
-if object_id('tempdb..#sa_int1') is not null drop table #sa_int1
+if object_id('tempdb..#students_taking_alg2') is not null drop table #students_taking_alg2
 
 select distinct
-sr.studentid
+sr.studentid,
+b.ethnic_cde,
+b.iep_spec_ed_flg,
+b.lep_flg
 
-into #sa_int1
+into #students_taking_alg2
 
 from student_request as sr
 
@@ -114,12 +130,15 @@ and b.status='A'
 
 
 ---[3b] active cohort r students with algebra 2 on their transcripts
-if object_id('tempdb..#sa_int2') is not null drop table #sa_int2
+if object_id('tempdb..#students_taken_alg2') is not null drop table #students_taken_alg2
 
 select distinct
 s.studentid
+b.ethnic_cde,
+b.iep_spec_ed_flg,
+b.lep_flg
 
-into #sa_int2
+into #students_taken_alg2
 
 from student_marks as s
 
@@ -135,33 +154,43 @@ and b.status='A'
 
 
 
----[3c] active cohort r students who have taken or are taking algebra 2
+---[3c] active cohort r students who have taken or are taking algebra 2 excluding irrelevant populations
 --[3ci] active cohort r students who have taken or are taking algebra 2
-if object_id('tempdb..#sa_int3') is not null drop table #sa_int3
+if object_id('tempdb..#students_alg2_int') is not null drop table #students_alg2_int
 
 select
-studentid
+studentid,
+ethnic_cde,
+iep_spec_ed_flg,
+lep_flg
 
-into #sa_int3
+into #students_alg2_int
 
-from #sa_int1
+from #students_taking_alg2
 
 union
 
-select studentid
+select
+studentid,
+ethnic_cde,
+iep_spec_ed_flg,
+lep_flg
 
-from #sa_int2
+from #students_taken_alg2
 
 
 --[3cii] active cohort r students who have taken or are taking algebra 2 excluding irrelevant populations
-if object_id('tempdb..#sa_final') is not null drop table #sa_final
+if object_id('tempdb..#students_alg2_final') is not null drop table #students_alg2_final
 
 select
-studentid
+studentid,
+ethnic_cde,
+iep_spec_ed_flg,
+lep_flg
 
-into #sa_final
+into #students_alg2_final
 
-from #sa_int3
+from #students_alg2_int
 
 where
 studentid not in	(
@@ -181,18 +210,21 @@ studentid not in	(
 
 
 -----[4] active cohort r students
-if object_id('tempdb..#s') is not null drop table #s
+if object_id('tempdb..#student_list') is not null drop table #student_list
 
 select distinct
-student_id
+student_id,
+ethnic_cde,
+iep_spec_ed_flg,
+lep_flg
 
-into #s
+into #student_list
 
 from bio_data
 
 where
-status='A'
-and grd9_entry_cde='R'
+grd9_entry_cde='R'
+and status='A'
 and student_id not in	(
 				select distinct b.id 
 				from spr_bio_data as b
@@ -210,51 +242,47 @@ and student_id not in	(
 
 
 -----[5] various demographic breakdowns for active cohort r students who have taken or are taking algebra 2
----[5a] race breakdown for active cohort r students who have taken or are taking algebra 2 
+---[5a] ethnic breakdown for active cohort r students who have taken or are taking algebra 2
+if object_id('tempdb..#ct_ethnicity_alg2') is not null drop table #ct_ethnicity_alg2
+
 select
-case when b.ethnic_cde in ('2','C','D') then 'Asian'
-when b.ethnic_cde in ('3','A') then 'Hispanic'
-when b.ethnic_cde in ('4','E') then 'Black'
-when b.ethnic_cde in ('5','F') then 'White'
+case when ethnic_cde in ('2','C','D') then 'Asian'
+when ethnic_cde in ('3','A') then 'Hispanic'
+when ethnic_cde in ('4','E') then 'Black'
+when ethnic_cde in ('5','F') then 'White'
 else 'Other'
 end as ethnicity,
-count(distinct s.studentid) as ct_students
+count(distinct studentid) as ct_students_alg2
 
-from #sa_final as s
+into #ct_ethnicity_alg2
 
-inner join bio_data as b
-on b.student_id=s.studentid
-
-where
-b.status='A'
+from #students_alg2_final
 
 group by 
-case when b.ethnic_cde in ('2','C','D') then 'Asian'
-when b.ethnic_cde in ('3','A') then 'Hispanic'
-when b.ethnic_cde in ('4','E') then 'Black'
-when b.ethnic_cde in ('5','F') then 'White'
+case when ethnic_cde in ('2','C','D') then 'Asian'
+when ethnic_cde in ('3','A') then 'Hispanic'
+when ethnic_cde in ('4','E') then 'Black'
+when ethnic_cde in ('5','F') then 'White'
 else 'Other'
 end
 
 
 
 ---[5b] iep status breakdown for active cohort r students who have taken or are taking algebra 2
+if object_id('tempdb..#ct_iep_alg2') is not null drop table #ct_iep_alg2
+
 select
-case when b.iep_spec_ed_flg='Y' then 'Student with Disabilities (IEP)'
+case when iep_spec_ed_flg='Y' then 'Student with Disabilities (IEP)'
 else NULL
 end as iep_status,
-count(distinct s.studentid) as ct_students
+count(distinct studentid) as ct_students_alg2
 
-from #sa_final as s
+into #ct_iep_alg2
 
-inner join bio_data as b
-on b.student_id=s.studentid
-
-where
-b.status='A'
+from #students_alg2_final
 
 group by 
-case when b.iep_spec_ed_flg='Y'
+case when iep_spec_ed_flg='Y'
 then 'Student with Disabilities (IEP)'
 else NULL
 end
@@ -262,24 +290,22 @@ end
 
 
 ---[5c] ell status breakdown for active cohort r students who have taken or are taking algebra 2
+if object_id('tempdb..#ct_ell_alg2') is not null drop table #ct_ell_alg2
+
 select
-case when b.lep_flg='Y' then 'ELL'
-when b.lep_flg='P' then 'Former ELL'
+case when lep_flg='Y' then 'ELL'
+when lep_flg='P' then 'Former ELL'
 else NULL
 end as ell_status,
-count(distinct s.studentid) as ct_students
+count(distinct studentid) as ct_students_alg2
 
-from #sa_final as s
+into #ct_ell_alg2
 
-inner join bio_data as b
-on b.student_id=s.studentid
-
-where
-b.status='A'
+from #students_alg2_final
 
 group by
-case when b.lep_flg='Y' then 'ELL'
-when b.lep_flg='P' then 'Former ELL'
+case when lep_flg='Y' then 'ELL'
+when lep_flg='P' then 'Former ELL'
 else NULL
 end
 
@@ -288,48 +314,43 @@ end
 
 
 -----[6] various demographic breakdowns for active cohort r students						
----[6a] race breakdown for active cohort r students
+---[6a] ethnic breakdown for active cohort r students
+if object_id('tempdb..#ct_ethnicity') is not null drop table #ct_ethnicity
+
 select
-case when b.ethnic_cde in ('2','C','D') then 'Asian'
-when b.ethnic_cde in ('3','A') then 'Hispanic'
-when b.ethnic_cde in ('4','E') then 'Black'
-when b.ethnic_cde in ('5','F') then 'White'
+case when ethnic_cde in ('2','C','D') then 'Asian'
+when ethnic_cde in ('3','A') then 'Hispanic'
+when ethnic_cde in ('4','E') then 'Black'
+when ethnic_cde in ('5','F') then 'White'
 else 'Other'
 end as ethnicity,
 count(distinct b.student_id) as ct_students
 
-from #s as s
+into #ct_ethnicity
 
-inner join bio_data as b
-on b.student_id=s.student_id
+from #student_list
 
-where
-b.status='A'
-
-group by
-case when b.ethnic_cde in ('2','C','D') then 'Asian'
-when b.ethnic_cde in ('3','A') then 'Hispanic'
-when b.ethnic_cde in ('4','E') then 'Black'
-when b.ethnic_cde in ('5','F') then 'White'
+case when ethnic_cde in ('2','C','D') then 'Asian'
+when ethnic_cde in ('3','A') then 'Hispanic'
+when ethnic_cde in ('4','E') then 'Black'
+when ethnic_cde in ('5','F') then 'White'
 else 'Other'
 end
 
 
 
 ---[6b] iep status breakdown for active cohort r students
+if object_id('tempdb..#ct_iep') is not null drop table #ct_iep
+
 select 
-case when b.iep_spec_ed_flg='Y' then 'Student with Disabilities (IEP)'
+case when iep_spec_ed_flg='Y' then 'Student with Disabilities (IEP)'
 else NULL
 end as iep_status,
-count(distinct b.student_id) as ct_students
+count(distinct student_id) as ct_students
 
-from #s as s
+into #ct_iep
 
-inner join bio_data as b
-on b.student_id=s.student_id
-
-where
-b.status='A'
+from #student_list
 
 group by
 case when b1.iep_spec_ed_flg='Y' then 'Student with Disabilities (IEP)'
@@ -339,23 +360,75 @@ end
 
 
 ---[6c] ell status breakdown for active cohort r students
+if object_id('tempdb..#ct_ell') is not null drop table #ct_ell
+
 select 
-case when b.lep_flg='Y' then 'ELL'
-when b.lep_flg='P' then 'Former ELL'
+case when lep_flg='Y' then 'ELL'
+when lep_flg='P' then 'Former ELL'
 else NULL
 end as ell_status,
-count(distinct b.student_id) as ct_students
+count(distinct student_id) as ct_students
 
-from #s as s
+into #ct_ell
 
-inner join bio_data as b
-on b.student_id=s.student_id
-
-where
-b.status='A'
+from #student_list
 
 group by
-case when b.lep_flg='Y' then 'ELL'
-when b.lep_flg='P' then 'Former ELL'
+case when lep_flg='Y' then 'ELL'
+when lep_flg='P' then 'Former ELL'
 else NULL
 end
+
+
+
+
+
+-----[7] various measures of algebra 2 availability for cohort r students
+---[7a] percentage of schools offering high school level alg2 in 2014
+select
+sa.ct_schools_alg2,
+s.ct_schools,
+sa.ct_schools_alg2/s.ct_schools as perc_schools_alg2
+
+from #schools_alg2 as sa
+
+cross join #schools as s
+
+
+
+---[7b] percentages of active cohort r students who have taken or are taking alg2 by race
+select
+ea.ct_students_alg2,
+e.ct_students,
+ea.ct_students_alg2/e.ct_students as perc_students_alg2
+
+from #ct_ethnicity_alg2 as ea
+
+right join #ct_ethnicity as e
+on e.ethnicity=ea.ethnicity
+
+
+
+---[7c] percentages of active cohort r students who have taken or are taking alg2 by iep status
+select
+ia.ct_students_alg2,
+i.ct_students,
+ia.ct_students_alg2/i.ct_students as perc_alg2_students
+
+from #ct_iep_alg2 as ia
+
+right join #ct_iep as i
+on i.iep_status=ia.iep_status
+
+
+
+---[7d] percentages of active cohort r students who have taken or are taking alg2 by ell status
+select
+ea.ct_students_alg2,
+e.ct_students,
+ea.ct_students_alg2/e.ct_students as perc_alg2_students
+
+from #ct_ell_alg2 as ea
+
+right join #ct_ell as e
+on e.ell_status=ea.ell_status
